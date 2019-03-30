70b22dc1105ed467f7191bae0edfccdcb5464406
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-03-30 16:07:12 -0300

    primeiro template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {"name" : "Vin", "power" : "Mistborn"}
+    return render_template('index.html', title='Home', user=user)
+
+
