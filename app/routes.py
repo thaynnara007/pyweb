92f1b14de1e17b7b9cc1cbadb0fdf7a418a9f51f
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-03-30 16:37:36 -0300

    mais um template

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,18 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {"name" : "Vin", "power" : "Mistborn"}
-    return render_template('index.html', title='Home', user=user)
+    user = {"username" : "Vin", "power" : "Mistborn"}
+    posts = [
+        {
+            'author':{"username":"Kelsier"},
+            'body': "Let's defeat the Lord Ruler!",
+        },
+        {
+            'author':{'username':'Elend'},
+            'body': 'Lets resolve this with a discussion'
+        }
+    ]
+
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
