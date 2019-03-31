04efedc8bf7275c8debb264440c93e7ac18b61f7
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-03-31 17:17:08 -0300

    adicionando primeiro schema

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
--- config.py
+++ config.py
@@ -1,24 +1,29 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ.get("SECRET_KEY") or "The eleventh metal"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://localhost:5432/swabook_db1"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class ProductionConfig(Config):
     DEBUG = False
+
 
 class StagingConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
 
+
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+
 
 class TestingConfig(Config):
     TESTING = True
