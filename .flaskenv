6743c776f0f14f7196a4fb1887230d3f17e29d2d
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-03-31 15:27:20 -0300

    configurando postgres

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=microblog.py
+DATABASE_RUL = "postgresql://localhost/swabook_db1"
--- config.py
+++ config.py
@@ -1,5 +1,24 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.environ.get("SECRET_KEY") or "The eleventh metal"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
