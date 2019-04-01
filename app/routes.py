1f54bc357ef9b9535e0d2d905dce3a91ffed90a7
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-01 20:27:46 -0300

    add routa para registro

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
+from app import db
 from app import app
 from flask import request
 from app.forms import LoginForm
 from app.models.user import User
 from werkzeug.urls import url_parse
 from flask_login import logout_user
+from app.forms import RegistrationForm
 from flask_login import login_required
 from flask_login import current_user, login_user
 from flask import render_template, flash, redirect, url_for
@@ -47,7 +49,7 @@ def login():
             login_user(user, remember=remember)
             next_page = request.args.get('next')
 
-            if(next_page or url_parse(next_page).netloc == ''): next_page = url_for('index')
+            if(not next_page or url_parse(next_page).netloc != ''): next_page = url_for('index')
             return redirect(next_page)
 
     return render_template('login.html', title='Sign In', form=form)
@@ -56,3 +58,24 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if (current_user.is_authenticated): return redirect(url_for('index'))
+    
+    form = RegistrationForm()
+
+    if(form.validate_on_submit()):
+
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        new_user = User(username=username, email=email)
+        new_user.set_password(password)
+
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Congratulations, you are now are register user!')
+        return redirect(url_for('login'))
+    
+    return render_template('register.html', title="Register", form=form)
