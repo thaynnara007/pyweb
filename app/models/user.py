36cdcc1c885993be3fd7707847d34a450325e57c
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-01 17:46:11 -0300

    implementado rota de login

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,14 @@
-from datetime import datetime
 from app import db
+from app import login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
   __tablename__ = "user"
 
   id = db.Column(db.Integer, primary_key=True)
@@ -10,5 +17,12 @@ class User(db.Model):
   password_hash = db.Column(db.String(128))
   posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+  def set_password(self, password):
+    self.password_hash = generate_password_hash(password)
+  
+  def check_password(self, password):
+    return check_password_hash(self.password_hash, password)
+
   def __repr__(self):
     return '<User {}>'.format(self.username)   
+
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
+from app.models.user import User
+from flask_login import current_user, login_user
+from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
@@ -22,12 +24,23 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
     
-    if( form.validate_on_submit()):
-        flash('Login requested for user {}, remember_me = {}'.format(
-            form.username, form.remember_me.data
-        ))
-        return redirect(url_for('index'))
+    if (current_user.is_authenticated): return redirect(url_for('index'))
 
+    form = LoginForm()
+
+    if (form.validate_on_submit()):
+
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember_me.data
+        user = User.query.filter_by(username=username).first()
+
+        if (user is None or not user.check_password(password)):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        else:
+            login_user(user, remember=remember)
+            return redirect(url_for('index'))
+            
     return render_template('login.html', title='Sign In', form=form)
