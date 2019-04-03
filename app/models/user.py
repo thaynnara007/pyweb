8706f713b6adf427c7564a98e37fb3467cd4b0c1
Thaynnara007 <thaynnara.goncalves@ccc.ufcg.edu.br>
2019-04-03 14:31:31 -0300

    adicionando follwers e migrando data base

--- app/models/user.py
+++ app/models/user.py
@@ -5,10 +5,15 @@ from datetime import datetime
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
+followers = db.Table('followers',
+  db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+  db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))    
+)
+
 @login.user_loader
 def load_user(id):
   return User.query.get(int(id))
-
 
 class User(UserMixin, db.Model):
   __tablename__ = "user"
@@ -20,6 +25,12 @@ class User(UserMixin, db.Model):
   about_me = db.Column(db.String(140))
   last_seen = db.Column(db.DateTime, default=datetime.utcnow)
   posts = db.relationship('Post', backref='author', lazy='dynamic')
+  followed = db.relationship(
+    'User', secondary=followers,
+    primaryjoin=(followers.c.follower_id == id),
+    secondaryjoin=(followers.c.followed_id == id),
+    backref=db.backref('followers', lazy='dynamic',), lazy='dynamic'
+    )
 
   def set_password(self, password):
     self.password_hash = generate_password_hash(password)
